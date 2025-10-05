24f44faac1b82c9d9a1cfbc9e8a72a3a74ff37e1
locleabcd <locleabcd@gmail.com>
2025-10-05 18:42:40 +0700

    fix: all

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common'
+import { AppService } from './app.service'
+import { IsPublic } from './shared/decorators/auth.decorator'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @IsPublic()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello()
   }
 }
