74b6aa2474e28f75739515d97d98c84998157bf2
leducloc-hcm <locleabcd@gmail.com>
2025-10-07 20:13:18 +0700

    func: Notification with socket

---
+++ src/routes/notification/notification.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common'
+import { CreateNotificationDto, UpdateNotificationDto } from './notification.dto'
+import { NotificationGateway } from '../../websockets/notification.gateway'
+import { NotificationRepository } from 'src/routes/notification/notification.repo'
+
+@Injectable()
+export class NotificationService {
+  constructor(
+    private readonly notificationGateway: NotificationGateway,
+    private readonly notificationRepository: NotificationRepository,
+  ) {}
+
+  async create(data: CreateNotificationDto) {
+    const notification = await this.notificationRepository.create(data)
+    this.notificationGateway.sendNotification(notification.userId, notification)
+    return notification
+  }
+
+  async findAll(userId: number) {
+    return this.notificationRepository.findAll(userId)
+  }
+
+  async findOne(id: number) {
+    return this.notificationRepository.findOne(id)
+  }
+
+  async update(id: number, data: UpdateNotificationDto) {
+    return this.notificationRepository.update(id, data)
+  }
+  async markAllAsRead(userId: number) {
+    return this.notificationRepository.markAllAsRead(userId)
+  }
+
+  async remove(id: number) {
+    return this.notificationRepository.remove(id)
+  }
+}
--- src/websockets/notification.gateway.ts
+++ src/websockets/notification.gateway.ts
@@ -1,7 +1,19 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets'
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets'
+import { Server } from 'socket.io'
+
 @WebSocketGateway({
-  cors: {
-    origin: '*',
-  },
+  namespace: 'notification',
 })
-export class NotificationGateway {}
+export class NotificationGateway {
+  @WebSocketServer()
+  server: Server
+
+  @SubscribeMessage('message')
+  handleMessage(@MessageBody() message: string): string {
+    return message
+  }
+
+  sendNotification(userId: number, notification: any) {
+    this.server.to(`user_${userId}`).emit('notification', notification)
+  }
+}
