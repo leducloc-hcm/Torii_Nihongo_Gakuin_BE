fc803be385904b408889ce576275576519b45f71
locleabcd <locleabcd@gmail.com>
2025-10-16 21:28:15 +0700

    fix: chat message and raise hand

--- src/websockets/websockets.module.ts
+++ src/websockets/websockets.module.ts
@@ -1,22 +1,12 @@
 import { Module } from '@nestjs/common'
 import { NotificationGateway } from './notification.gateway'
-import { WebRtcGateway } from './webrtc.gateway'
-import { WebRtcService } from './webrtc.service'
 import { JanusService } from './janus/janus.service'
 import { JanusWebSocketManager } from './janus/janus-websocket.manager'
-import { OnlineClassManagementService } from './online-class-management.service'
 import { S3Service } from '../shared/services/s3.service'
+import { WebRTCGateway } from './webrtc.gateway'
 
 @Module({
-  providers: [
-    NotificationGateway,
-    WebRtcGateway,
-    WebRtcService,
-    JanusService,
-    JanusWebSocketManager,
-    OnlineClassManagementService,
-    S3Service,
-  ],
-  exports: [NotificationGateway, WebRtcGateway, WebRtcService, JanusService, OnlineClassManagementService],
+  providers: [NotificationGateway, WebRTCGateway, JanusService, JanusWebSocketManager, S3Service],
+  exports: [NotificationGateway, JanusService],
 })
 export class WebsocketsModule {}
